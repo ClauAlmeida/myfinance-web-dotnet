16a9ece7f0a1ff8546af8fb506177b8fdd4f8ca7
Claudia Almeida <claudiacristina.r.dealmeida@gmail.com>
2023-07-22 18:43:42 -0300

    Updating project files

--- myfinance-web-dotnet/Controllers/HomeController.cs
+++ myfinance-web-dotnet/Controllers/HomeController.cs
@@ -1,31 +1,36 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using myfinance_web_dotnet.Models;
+using myfinance_web_netcore.Models;
 
-namespace myfinance_web_dotnet.Controllers;
-
-public class HomeController : Controller
+namespace myfinance_web_netcore.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    [Route("[controller]")]
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
+
+
